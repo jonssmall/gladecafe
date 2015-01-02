2c27f130c134d0d46b5abbdc2522e519bc626ff1
Jon Small <jonssmall@gmail.com>
2015-01-01 22:41:39 -0500

    Programmatically build photo grid using ngRepeat; use a function to set the same modal with different image/description

--- assets/script.js
+++ assets/script.js
@@ -36,6 +36,44 @@ myApp.config(function($routeProvider) {
 myApp.controller('menuController', function($scope) {
 	// create a message to display in our view
 	$scope.message = 'Everyone come and see how good I look!';
+	var cornBeef = {
+		description : "Home-made corned beef on sourdough.",
+		image: "photos/corned-beef.jpg"
+	}
+	var chickenClub = {
+		description : "Grilled chicken club sandwich with freshly diced coleslaw.",
+		image: "photos/chicken-club.jpg"
+	}
+	var hamYam = {
+		description: "Diced ham baked into a sweet potato.",
+		image: "photos/yam-ham.jpg"
+	}
+	var baconQuiche = {
+		description: "Bacon, spinach and cheese baked golden brown in an egg quiche.",
+		image: "photos/bacon-quiche.jpg"
+	}
+	var blueMangoPie = {
+		description: "Fresh mango slices and blueberries.",
+		image: "photos/blueberry-mango-pie.jpg"
+	}
+	var eggsBenedict = {
+		description: "Classic breakfast combination with ham and homemade hollandaise sauce.",
+		image: "photos/eggs-benedict.jpg"
+	}
+	var oreoCheesecake = {
+		description: "Crumbled oreo cookie baked into layers of creamy cheesecake.",
+		image: "photos/oreo-cheesecake.jpg"
+	}
+	var monteCristo = {
+		description: "Batter dipped bread with ham, egg, cheese and candied bacon.",
+		image: "photos/monte-cristo.jpg"
+	}
+	$scope.food = [cornBeef, chickenClub, hamYam, baconQuiche, blueMangoPie, eggsBenedict, oreoCheesecake, monteCristo]
+
+	$scope.setModal = function(foodItem) {	
+		$scope.currentPicture = foodItem.image;
+		$scope.currentDescription = foodItem.description;
+	}
 });
 
 myApp.controller('aboutController', function($scope) {
