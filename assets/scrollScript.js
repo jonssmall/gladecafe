e41b0ffa14200a456ce38569626562976c4d3716
Jon Small <jonssmall@gmail.com>
2015-05-03 16:06:59 -0400

    removed incorrect directory routing

--- assets/scrollScript.js
+++ assets/scrollScript.js
@@ -7,43 +7,43 @@ myApp.controller('menuController', function($scope) {
 	$scope.message = 'Everyone come and see how good I look!';
 	var cornBeef = {
 		description : "Home-made corned beef on sourdough.",
-		image: "/photos/corned-beef.jpg",
-		thumbnail: "/photos/thumbnails/corned-beef.jpg"
+		image: "photos/corned-beef.jpg",
+		thumbnail: "photos/thumbnails/corned-beef.jpg"
 	}
 	var chickenClub = {
 		description : "Grilled chicken club sandwich with freshly diced coleslaw.",
-		image: "/photos/chicken-club.jpg",
-		thumbnail: "/photos/thumbnails/chicken-club.jpg"
+		image: "photos/chicken-club.jpg",
+		thumbnail: "photos/thumbnails/chicken-club.jpg"
 	}
 	var hamYam = {
 		description: "Diced ham baked into a sweet potato.",
-		image: "/photos/yam-ham.jpg",
-		thumbnail: "/photos/thumbnails/yam-ham.jpg"
+		image: "photos/yam-ham.jpg",
+		thumbnail: "photos/thumbnails/yam-ham.jpg"
 	}
 	var baconQuiche = {
 		description: "Bacon, spinach and cheese baked golden brown in an egg quiche.",
-		image: "/photos/bacon-quiche.jpg",
-		thumbnail: "/photos/thumbnails/bacon-quiche.jpg"
+		image: "photos/bacon-quiche.jpg",
+		thumbnail: "photos/thumbnails/bacon-quiche.jpg"
 	}
 	var blueMangoPie = {
 		description: "Fresh mango slices and blueberries.",
-		image: "/photos/blueberry-mango-pie.jpg",
-		thumbnail: "/photos/thumbnails/blueberry-mango-pie.jpg"
+		image: "photos/blueberry-mango-pie.jpg",
+		thumbnail: "photos/thumbnails/blueberry-mango-pie.jpg"
 	}
 	var eggsBenedict = {
 		description: "Classic breakfast combination with ham and homemade hollandaise sauce.",
-		image: "/photos/eggs-benedict.jpg",
-		thumbnail: "/photos/thumbnails/eggs-benedict.jpg"
+		image: "photos/eggs-benedict.jpg",
+		thumbnail: "photos/thumbnails/eggs-benedict.jpg"
 	}
 	var oreoCheesecake = {
 		description: "Crumbled oreo cookie baked into layers of creamy cheesecake.",
-		image: "/photos/oreo-cheesecake.jpg",
-		thumbnail: "/photos/thumbnails/oreo-cheesecake.jpg"
+		image: "photos/oreo-cheesecake.jpg",
+		thumbnail: "photos/thumbnails/oreo-cheesecake.jpg"
 	}
 	var monteCristo = {
 		description: "Batter dipped bread with ham, egg, cheese and candied bacon.",
-		image: "/photos/monte-cristo.jpg",
-		thumbnail: "/photos/thumbnails/monte-cristo.jpg"
+		image: "photos/monte-cristo.jpg",
+		thumbnail: "photos/thumbnails/monte-cristo.jpg"
 	}
 	$scope.food = [cornBeef, chickenClub, hamYam, baconQuiche, blueMangoPie, eggsBenedict, oreoCheesecake, monteCristo]
 
